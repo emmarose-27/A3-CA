c824be15b52cbfa452960f48eb3385247aad1e99
Emma Rosenthal <124089752+emmarose-27@users.noreply.github.com>
2024-02-04 22:34:25 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+  "dojo/domReady!"
+], function (Map, MapView, FeatureLayer) {
+  /************************************************************
+   * Creates a new WebMap instance. A WebMap must reference
+   * a PortalItem ID that represents a WebMap saved to
+   * arcgis.com or an on-premise portal.
+   *
+   * To load a WebMap from an on-premise portal, set the portal
+   * url with esriConfig.portalUrl.
+   ************************************************************/
+  const symbol = {
+    type:"simple-marker",
+    color:"red",
+    size: 4
+  };
+  
+  const renderer = {
+    type: "simple",
+    symbol: symbol    
+  };
+  
+  const template = {
+    title:"What Happened?",
+    type:"fields",
+    fieldInfos:[{
+      fieldName: "CollisnTyp",
+      label: "Collision Type",
+      visible: true
+    }]
+  }
+  
+  const fl = new FeatureLayer({
+    url: "https://services2.arcgis.com/zNjnZafDYCAJAbN0/arcgis/rest/services/Traffic_Collisions/FeatureServer",
+    renderer: renderer,
+    outFields: ["*"],
+    popupTemplate: template
+  });
+
+  /************************************************************
+   * Set the WebMap instance to the map property in a MapView.
+   ************************************************************/
+  var map = new Map({
+    basemap: "streets"
+  });
+
+  var view = new MapView({
+    container: "viewDiv",
+    map: map,
+    extent: {
+      // autocasts as new Extent()
+      // xmin: -118.264858634618,
+      // ymin: 33.5444932701483,
+      // xmax: -117.299011374275,
+      // ymax: 35.0054716855699,
+      xmin: -118.19876123,
+      ymin: 34.1981025,
+      xmax: -118.05517288,
+      ymax: 34.11379905,
+      spatialReference: 4326
+    }
+  });
+
+  map.add(fl);
+});
